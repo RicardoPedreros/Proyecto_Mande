6ded37e4dfe36dffb6822ac14d2b68af9381a906
KevinL13 <kevin.loaiza@correounivalle.edu.co>
2020-05-19 21:40:34 -0500

    mapa componente

--- Mande/cliente/src/Routes.js
+++ Mande/cliente/src/Routes.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import registrarUsuario from './components/registrarUsuario';
 import RegistrarTrabajador from './components/registrarTrabajador';
-import Mapa from './components/mapa';
-import Mapacomponente from './components/mapacomponente';
+import MapaComponente from './components/mapaComponente';
 
 import InputLaborNombre from './components/inputLabor';
 import ListLabores from './components/listLabor';
@@ -15,8 +14,8 @@ const Routes = () =>{
            <Route exact path= '/' component= {RegistrarTrabajador} />
            <Route exact path= '/registrarusuario' component= {registrarUsuario} />
            
-           <Route exact path= '/mapa' component= {Mapa} />
-           <Route exact path= '/mapacomponente' component= {Mapacomponente} />
+         
+           <Route exact path= '/mapa' component= {MapaComponente} />
 
            <Route exact path= '/registrarlabor' component= {InputLaborNombre} />
            <Route exact path= '/listarbores' component= {ListLabores} />
