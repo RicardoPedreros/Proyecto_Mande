f839ea4a445b65a8347f77357468e302c1c2a745
pascu999 <cristian.pascumal@correounivalle.edu.co>
2020-05-23 17:20:52 -0500

    mejorando vista de mapa

--- Mande/cliente/src/components/mapa.js
+++ Mande/cliente/src/components/mapa.js
@@ -17,13 +17,11 @@ function Map() {
   const [selectedUsuario, setSelectedUsuario] = useState(null);
   const usuario_celular = localStorage.getItem('usuario_celular')
   const trabajador_documento = localStorage.getItem('trabajador_documento')
-  const [latitud,setLatitud] = useState('');
-  const [longitud,setLongitud] = useState('');
-  
 
 
 
-   const getTrabajador = async () => {
+
+  const getTrabajador = async () => {
     try {
       const body = { trabajador_documento: trabajador_documento }
       const response = await fetch("http://localhost:5000/InformacionTrabajador", {
@@ -39,9 +37,9 @@ function Map() {
 
       if (jsonData) {
         setTrabajador(jsonData.rows[0]);
-        
+
         console.log(trabajador);
-        
+
 
       }
 
@@ -54,7 +52,7 @@ function Map() {
   };
 
 
- 
+
 
   const getUsuario = async () => {
     try {
@@ -72,9 +70,6 @@ function Map() {
 
       if (jsonData) {
         setUsuario(jsonData.rows[0]);
-        setLatitud(jsonData.rows[0].usuario_latitud);
-        setLongitud(jsonData.rows[0].usuario_longitud);
-        
 
       }
 
@@ -89,7 +84,7 @@ function Map() {
   useEffect(() => {
     getUsuario();
     getTrabajador();
-  },[]);
+  }, []);
 
 
 
@@ -108,7 +103,7 @@ function Map() {
           lng: parseFloat(usuario.usuario_longitud)
         }}
         onClick={() => {
-          
+
           setSelectedTrabajador(null);
           setSelectedUsuario(usuario);
         }}
@@ -127,7 +122,7 @@ function Map() {
           lng: parseFloat(trabajador.trabajador_longitud)
         }}
         onClick={() => {
-          {console.log(usuario.usuario_nombre)}
+          { console.log(usuario.usuario_nombre) }
           setSelectedTrabajador(trabajador);
           setSelectedUsuario(null);
         }}
@@ -200,14 +195,30 @@ const ApiKey = 'AIzaSyDtDvezVGJgaFMqa8FboBS4dcR6QKfnMyw';
 
 export default function Mapa() {
   return (
-    <div style={{ width: "40vw", height: "10vh" }}>
-      MAPA MANDE
-      <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${ApiKey}`}
-        loadingElement={<p>   Cargando </p>}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+
+    <div className="container shadow-lg p-5 mb-4 bg-white rounded" style={{ width: "50vw", height: "1000vh", border: "solid", borderBlockColor: "black", margin: "0 auto" }} >
+      <div className="container p4 text-center">
+        <h1 style={{ color: "#a02e2e", }}><b>Solicitud de Servicio</b></h1>
+        <div className="mt-5 container   mb-4 bg-white rounded" style={{ width: "40vw", height: "80vh", border: "solid", borderBlockColor: "black", margin: "0 auto", borderRadius: "20" }} >
+          <div className="text-left">
+          <h5 className="mt-4 ">Descripción:</h5>
+          </div>
+          <div className="text-center">
+            <input style={{ width: "30vw", height: "10vh" ,margin: "0 auto"}} type="text" name="Descripcion" className="form-control mt-2 " />
+          </div>
+          <MapWrapped
+
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${ApiKey}`}
+            loadingElement={<p>   Cargando </p>}
+            containerElement={<div className="mt-5" style={{ width: "400px", height: `200px`, margin: "0 auto" }} />}
+            mapElement={<div style={{ height: `200%`, margin: "0 auto" }} />}
+          />
+
+
+
+        </div>
+
+      </div>
     </div>
   );
 }
