559a65b0472a350e25ae3ed301b780cc4501e3ef
pascu999 <cristian.pascumal@correounivalle.edu.co>
2020-05-24 23:36:59 -0500

    fusionando cambios

--- Mande/Server/db.js
+++ Mande/Server/db.js
@@ -4,11 +4,8 @@ const Pool = require("pg").Pool;
 const pool = new Pool ({
     user:"postgres",
     password:"11111",
-<<<<<<< HEAD
     host:"localhost", // cambiar a "database" cuando se haga deploy con docker-compose
-=======
-    host:"172.17.0.1", //  cambiar a "database" cuando se haga deploy con docker-compose
->>>>>>> 9b02e361e702405c93cce7afceb97367c9c93313
+   // host:"172.17.0.1", //  cambiar a "database" cuando se haga deploy con docker-compose
     port:5432,
     database: "mande"
 
